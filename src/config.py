c47077f83e8f77b8318ad5aaf89a5163bc59de9c
sidney092k@gmail.com <sidney092k@gmail.com>
2023-11-26 19:37:45 -0300

    basico funcionando

---
+++ src/config.py
@@ -0,0 +1,15 @@
+import pygame
+import random
+
+pygame.init()
+
+# cores em RGB para utilizar
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 128, 0)
+
+#taxa de quadros
+FPS = 24
+
+SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
